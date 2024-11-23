66ec404e116d242777ca4b49428df92acd84df88
Pablo Fornali <pablofornalimanzanas@gmail.com>
2024-11-23 19:03:29 +0100

    Icons Colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,10 @@
 import defaultTheme from 'tailwindcss/defaultTheme'
 
+const iconsColors = {
+	"github": "#d1d7e0",
+	"linkedin": "#0e76a8",
+	"bluesky": "#1185fe",
+}
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -9,10 +14,22 @@ export default {
 			fontFamily: {
 				'sans': ['"Lato"', ...defaultTheme.fontFamily.sans],
 			},
+			colors: {
+				github: iconsColors.github,
+				linkedin: iconsColors.linkedin,
+				bluesky: iconsColors.bluesky,
+			}
 		}
 	},
-	
+	safelist: [
+		{
+			pattern: /(stroke|fill|text)-(github|linkedin|bluesky)/,
+			variants: ['hover', 'focus'],
+		},
+	],
+
 	plugins: [
 		require('@tailwindcss/typography'),
 	],
+
 }
